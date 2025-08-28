a08455f18a0fcd7057d9544fbf91c27cc16316b1
Pradeep PVN <pvn.pradeep@gmail.com>
2025-08-28 15:31:55 +0530

    Added stack implementation

---
+++ src/stack.py
@@ -0,0 +1,10 @@
+## Stack implementation
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop() if self.items else None
---
+++ tests/test_stack.py
@@ -0,0 +1,6 @@
+from src.stack import Stack
+
+def test_stack_push_pop():
+    s = Stack()
+    s.push(10)
+    assert s.pop() == 10
