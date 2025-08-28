d1872f084a9aa660b272ac5151aa0f2e1a80efdd
Pradeep PVN <pvn.pradeep@gmail.com>
2025-08-28 15:56:01 +0530

    re-formatted with docstring

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
 def reverse_string(s: str) -> str:
+    """_summary_
+
+    Args:
+        s (str): _description_
+
+    Returns:
+        str: _description_
+    """    
     return s[::-1]
--- src/stack.py
+++ src/stack.py
@@ -1,9 +1,20 @@
 class Stack:
+    """ """
+
     def __init__(self):
         self.items = []
 
     def push(self, item):
+        """
+
+        Args:
+          item:
+
+        Returns:
+
+        """
         self.items.append(item)
 
     def pop(self):
+        """ """
         return self.items.pop() if self.items else None
